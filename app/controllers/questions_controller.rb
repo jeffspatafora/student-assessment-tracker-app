623495cdd0cac859cb55b6ca168907dae889c3fb
jeffspatafora <jeffspatafora@gmail.com>
2022-05-13 11:12:46 -0700

    adds session_token_request to questions index and string  interpolates into question request

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,13 @@
 class QuestionsController < ApplicationController
-  def index
-    response = HTTP.get("https://opentdb.com/api.php?amount=1&category=19&difficulty=hard")
+  def index 
+
+    session_token_request = HTTP.get("https://opentdb.com/api_token.php?command=request")
+    session_token_response = session_token_request.parse(:json)
+    session_token = session_token_response["token"]
+
+    response = HTTP.get("https://opentdb.com/api.php?amount=1&token=#{session_token}")
     questions = response.parse(:json)
     render json: {message: questions["results"][0]["question"]}
+
   end
 end

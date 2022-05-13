82b2efb119b754a6a4bcf073b8a70b5d862d1432
jeffspatafora <jeffspatafora@gmail.com>
2022-05-13 14:22:56 -0700

    adds variables for type, question, correct_answer, incorrect_answers for json output

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,9 +11,12 @@ class QuestionsController < ApplicationController
 
     response = HTTP.get("https://opentdb.com/api.php?amount=1&category=#{category}&difficulty=#{difficulty}&token=#{session_token}")
     questions_data = response.parse(:json)
+    type = questions_data["results"][0]["type"]
     question = questions_data["results"][0]["question"]
-    answer = questions_data["results"][0]["correct_answer"]
-    render json: {question: question, answer: answer}
+    correct_answer = questions_data["results"][0]["correct_answer"]
+    incorrect_answers = questions_data["results"][0]["incorrect_answers"]
+    
+    render json: {question: question, type: type, correct_answer: correct_answer, incorrect_answers: incorrect_answers}
 
   end
 end

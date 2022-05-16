d4fa869db51674dd80da1987729122b167d7142f
jeffspatafora <jeffspatafora@gmail.com>
2022-05-16 10:56:22 -0700

    adds all_answers variable to store all answers array on get_question action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,8 +14,17 @@ class QuestionsController < ApplicationController
     question = questions_data["results"][0]["question"]
     correct_answer = questions_data["results"][0]["correct_answer"]
     incorrect_answers = questions_data["results"][0]["incorrect_answers"]
+    all_answers = []
+    all_answers << correct_answer
+
+    i = 0
+    while i < incorrect_answers.length
+      all_answers << incorrect_answers[i]
+      i += 1
+    end
+
     
-    render json: {question: question, type: type, correct_answer: correct_answer, incorrect_answers: incorrect_answers}
+    render json: {question: question, type: type, all_answers: all_answers, correct_answer: correct_answer, incorrect_answers: incorrect_answers}
 
   end
 end

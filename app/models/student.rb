bffd9861f56cc04b06d881692a678eccbe8390df
jeffspatafora <jeffspatafora@gmail.com>
2022-05-17 23:47:25 -0700

    adds associations for student and user to student and user models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ApplicationRecord
   has_many :user_notes
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :user_notes
+  has_many :students
 
   has_secure_password
   validates :email, presence: true, uniqueness: true

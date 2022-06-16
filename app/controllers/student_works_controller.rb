66fc250d40101b851ed5d4d8d16aa713b340644e
jeffspatafora <jeffspatafora@gmail.com>
2022-06-15 22:48:29 -0700

    adds user_id, student_id, project_id params to student work create function, adds before_action :authenticate_user to StudentWorksController, tested on frontend for user_id

--- app/controllers/student_works_controller.rb
+++ app/controllers/student_works_controller.rb
@@ -1,9 +1,14 @@
 class StudentWorksController < ApplicationController
+  before_action :authenticate_user
+  
   def create
     # binding.pry
     response = Cloudinary::Uploader.upload(params[:image], resource_type: :auto)
     cloudinary_url = response["secure_url"]
     student_work = StudentWork.create!(
+      user_id: current_user.id,
+      student_id: params[:student_id],
+      project_id: params[:project_id],
       image: cloudinary_url
     )
 

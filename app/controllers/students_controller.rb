baaefaa5281e4e1f08e13360400e4dda0a1793dd
jeffspatafora <jeffspatafora@gmail.com>
2022-06-18 13:14:06 -0700

    adds cloudinary_image_url and cloudinary_image_public_id to student create action tests in request.http

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,10 +7,16 @@ class StudentsController < ApplicationController
   end
 
   def create
+    response = Cloudinary::Uploader.upload(params[:image])
+    cloudinary_image_url = response["secure_url"]
+    cloudinary_image_public_id = response["public_id"]
+
     student = Student.new(
       user_id: current_user.id,
       name: params[:name].downcase,
-      email: params[:email]
+      email: params[:email].downcase,
+      cloudinary_image_url: cloudinary_image_url,
+      cloudinary_image_public_id: cloudinary_image_public_id
     )
 
     student.save

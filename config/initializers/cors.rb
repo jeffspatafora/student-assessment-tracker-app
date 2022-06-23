cd74966096b00a6dd0e86944d8bbc6a742eddcb2
jeffspatafora <jeffspatafora@gmail.com>
2022-06-22 18:00:05 -0700

    adds drprofessorfrontend.herokuapp.com to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080"
+    origins "localhost:8080", "drprofessorfrontend.herokuapp.com"
 
     resource "*",
       headers: :any,
